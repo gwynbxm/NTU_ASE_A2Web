41c5cf381c24ffca34066837877721fe0c243114
NikhitaMenon <44311001+NikhitaMenon@users.noreply.github.com>
2020-11-16 18:53:11 +0800

    Update LoginCred.java

--- CZ3002_Grp3_LoginWebApp/src/grp3/ase/ntu/model/LoginCred.java
+++ CZ3002_Grp3_LoginWebApp/src/grp3/ase/ntu/model/LoginCred.java
@@ -28,14 +28,13 @@ public class LoginCred extends ActionSupport{
 		 * execute() function called upon login button pressed
 		 * @return
 		 */
-		public String execute() {
-			int a=grp3.ase.ntu.controller.ValidateLogin.verify(username,password);
-			if (a==1){
-				//System.out.println("yes");
-				return "success";
-			}
-			//System.out.println("no");
-			return "error";
+		public String execute(String username, String password) {
+			if((ValidateLogin.verify(username,password) == 1)
+			   {
+				 return "Successful Login";
+			   }
+			 else
+			   	execute(String username, String password);
 		}
 		
 }
